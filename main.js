ab52c26b95b9f0e614975927b17e9167158f76d2
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-18 23:29:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+let weather = "Cloudy";
+if (weather == "raining") {
+    console.log("wear a raincoat"); //false
+}
+else if (weather == "cloudy") {
+    console.log("wear a sunglasses"); //false
+}
+else {
+    console.log("wear a light jacket"); //true
+}
+let Reached = "school";
+if (Reached == "School") {
+    console.log("take class on time"); //false
+}
+else {
+    console.log("considered as absent"); //true
+}
